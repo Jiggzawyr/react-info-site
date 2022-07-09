4a087fcd1b564f0b29c25e6b2b3b471a10cc6e56
Jiggzawyr <55451683+Jiggzawyr@users.noreply.github.com>
2022-07-09 16:51:47 +0200

    add dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 
-export default function Navbar() {
+export default function Navbar(props) {
     return(
-        <nav>
-            <img className="nav-icon" src={require("../images/react-icon-small.png")} />
+        <nav className={props.darkMode ? "dark": ""}>
+            <img className="nav-icon" src={require("../images/react-icon-small.png")} alt="react-icon-small" />
             <h3 className="nav-logo-text">ReactFacts</h3>
-            <h4 className="nav-title">React Course - Project 1</h4>
+
+            <div className="toggler" >
+                <p className="toggler-light">Light</p>
+                <div className="toggler-slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler-slider-circle"></div>
+                </div>
+                <p className="toggler-dark">Dark</p>
+            </div>
+
         </nav>
     )
 }
